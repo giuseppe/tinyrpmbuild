c39d9fd6060eb9235d78b8eb61472d00bbe147c1
Giuseppe Scrivano <gscrivan@redhat.com>
2017-05-12 12:31:28 +0200

    tests: add another test
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -18,3 +18,10 @@ tinyrpmbuild.py $TMPDIR/root $TMPDIR/rpm.rpm magicpackage
 rpm -qlp $TMPDIR/rpm.rpm > list_files
 grep "/a/b/c/file" list_files
 grep "/a/b/c/file2" list_files
+
+rm -rf $TMPDIR/root
+mkdir -p $TMPDIR/root/dir
+dd if=/dev/urandom of=$TMPDIR/root/dir/bigfile count=20 bs=1M
+tinyrpmbuild.py $TMPDIR/root $TMPDIR/rpm.rpm packagebigfile
+rpm2cpio $TMPDIR/rpm.rpm | cpio -iv --to-stdout dir/bigfile > $TMPDIR/bigfileextracted
+cmp $TMPDIR/bigfileextracted $TMPDIR/root/dir/bigfile
