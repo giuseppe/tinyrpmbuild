2d77f6378938f7d5262ceb22388330e0d666dbac
Giuseppe Scrivano <gscrivan@redhat.com>
2017-05-15 18:01:10 +0200

    tests: add test
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -10,14 +10,21 @@ function cleanup {
 }
 trap cleanup EXIT
 
-mkdir -p $TMPDIR/root/a/b/c
+mkdir -p $TMPDIR/root/{etc,a}/b/c
 echo hello > $TMPDIR/root/a/b/c/file
 echo world > $TMPDIR/root/a/b/c/file2
+echo configuration > $TMPDIR/root/etc/a-conf-file
 
 tinyrpmbuild.py $TMPDIR/root $TMPDIR/rpm.rpm magicpackage
 rpm -qlp $TMPDIR/rpm.rpm > list_files
 grep "/a/b/c/file" list_files
 grep "/a/b/c/file2" list_files
+
+# rpm returns 1 for some reason, disable the errors for now...
+set +xeuo pipefail
+rpm -q --qf '[%{filenames}: %{fileflags}\n]' $TMPDIR/rpm.rpm > rpm_out
+set -xeuo pipefail
+grep -q "/etc/a-conf-file:.*1" rpm_out
 
 rm -rf $TMPDIR/root
 mkdir -p $TMPDIR/root/dir
