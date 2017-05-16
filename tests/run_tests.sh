09deb46f9b470740759687608f7835d0764e8f8c
Giuseppe Scrivano <gscrivan@redhat.com>
2017-05-16 10:45:43 +0200

    tests: try to install rpm in a container when running as root
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -32,3 +32,26 @@ dd if=/dev/urandom of=$TMPDIR/root/dir/bigfile count=20 bs=1M
 tinyrpmbuild.py $TMPDIR/root $TMPDIR/rpm.rpm packagebigfile
 rpm2cpio $TMPDIR/rpm.rpm | cpio -iv --to-stdout dir/bigfile > $TMPDIR/bigfileextracted
 cmp $TMPDIR/bigfileextracted $TMPDIR/root/dir/bigfile
+
+
+if test `id -u` != 0; then
+    echo "SKIP NON ROOT TESTS"
+    exit 0
+fi
+
+# Add more files to the rpm root and rebuild
+mkdir -p $TMPDIR/root/{etc,a}/b/c
+echo hello > $TMPDIR/root/a/b/c/file
+echo world > $TMPDIR/root/a/b/c/file2
+echo configuration > $TMPDIR/root/etc/a-conf-file
+
+tinyrpmbuild.py $TMPDIR/root $TMPDIR/rpm.rpm installablerpm
+
+# root only tests
+cat > $TMPDIR/script.sh  << EOF
+rpm -vi /tmp/rpm.rpm
+rpm -vV installablerpm
+rpm -e installablerpm
+EOF
+
+docker run --rm -ti -v $TMPDIR/rpm.rpm:/tmp/rpm.rpm:Z -v $TMPDIR/script.sh:/tmp/script.sh:Z fedora sh /tmp/script.sh
